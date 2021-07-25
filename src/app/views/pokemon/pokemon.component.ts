e55372ed040e2d08e2dd94fe983e5990dbfe2488
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-07-25 19:16:37 +0700

    get error

--- src/app/views/pokemon/pokemon.component.ts
+++ src/app/views/pokemon/pokemon.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Pokemon } from '@shared/interfaces/pokemon.interface';
+import { AppStore } from '@shared/interfaces/store.interface';
+import * as PokemonActions from '@states/pokemon/pokemon.actions';
+import * as PokemonSelectors from '@states/pokemon/pokemon.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PokemonComponent implements OnInit {
+export class PokemonComponent {
+  public pokemon: Pokemon = {} as Pokemon;
+  public pokemons$: Observable<any> = this.store$.select(
+    PokemonSelectors.selectAll
+  );
 
-  constructor() { }
+  constructor(private store$: Store<AppStore>) {
+    this.store$.dispatch(new PokemonActions.LoadPokemon());
+  }
 
-  ngOnInit(): void {
+  public onDelete(pokemon: Pokemon) {
+    this.store$.dispatch(new PokemonActions.Delete(pokemon.id));
+  }
+  public onSelect(pokemon: Pokemon) {
+    this.pokemon = pokemon;
+  }
+
+  public onUpdate(pokemon: Pokemon) {
+    this.store$.dispatch(new PokemonActions.Update(pokemon));
+  }
+  public onAdd(pokemon: Pokemon) {
+    this.store$.dispatch(new PokemonActions.Add(pokemon));
   }
 
 }
