1850819b959fa21064c95311e6fa340f09b836e4
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-07-25 19:36:07 +0700

    done!

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { PokemonEffects } from '@states/pokemon/pokemon.effects';
+import { reducers } from '@shared/states/root.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
-    // StoreModule.forRoot(reducer),
-    // EffectsModule.forRoot([PokemonEffects])
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([PokemonEffects])
   ],
   providers: [],
   exports: []
